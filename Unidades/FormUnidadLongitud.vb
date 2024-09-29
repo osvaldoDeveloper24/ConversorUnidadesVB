eb73468b65db6e04f1be25cc72fe5225da25ea36
Rudy Hernandez <2523166@clases.edu.sv>
2024-09-29 11:09:40 -0600

    Prubea..

--- Unidades/FormUnidadLongitud.vb
+++ Unidades/FormUnidadLongitud.vb
@@ -1,6 +1,5 @@
 ﻿Public Class FormUnidadLongitud
     Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
         Console.WriteLine("Hola Mundo")
-
     End Sub
 End Class
