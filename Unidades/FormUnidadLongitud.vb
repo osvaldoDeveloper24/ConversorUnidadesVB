cebda991a9f2de9e205cd648d938defe4c936acf
Rudy Hernandez <2523166@clases.edu.sv>
2024-09-29 15:26:35 -0600

    Conversor de unidades de longitud.

--- Unidades/FormUnidadLongitud.vb
+++ Unidades/FormUnidadLongitud.vb
@@ -1,5 +1,19 @@
 ﻿Public Class FormUnidadLongitud
+    Dim longitud As New Unidades.Longitud
+    Dim resultado As Double
+    'Dim resultadoRedondeado as Double 
     Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        longitud.Valor = Double.Parse(TextNumero.Text)
+        Dim cantidad As Double = longitud.Valor
 
+        longitud.UnidadBase = ComboUnidadBase.SelectedIndex
+        Dim opcionBase As Integer = longitud.UnidadBase
+
+        longitud.UnidadEquivalente = ComboUnidadEquivalencia.SelectedIndex
+        Dim opcionEquivalencia As Integer = longitud.UnidadEquivalente
+
+        resultado = longitud.convertir(cantidad, opcionBase, opcionEquivalencia)
+        'resultadoRedondeado = Math.Round(resultado, 2)
+        TextResultado.Text = resultado.ToString()
     End Sub
 End Class
