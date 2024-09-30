1e2f043341b3c23dd820d19260cff768ae70b0e5
Rudy Hernandez <2523166@clases.edu.sv>
2024-09-29 20:06:53 -0600

    Actualizacion de conversor de magnitudes longitudinales.

--- Unidades/Longitud.vb
+++ Unidades/Longitud.vb
@@ -16,109 +16,109 @@
                 If (opcionEquivalencia = 0) Then
                     respuesta = numero * 1 'Milimetro
                 ElseIf (opcionEquivalencia = 1) Then
-                    respuesta = numero / 0.1 'Centimetro
+                    respuesta = numero * 0.1 'Centimetro
                 ElseIf (opcionEquivalencia = 2) Then
-                    respuesta = numero / 0.0393701 'Pulgada
+                    respuesta = numero * 0.0393701 'Pulgada
                 ElseIf (opcionEquivalencia = 3) Then
-                    respuesta = numero / 0.00328084 'Pie
+                    respuesta = numero * 0.00328084 'Pie
                 ElseIf (opcionEquivalencia = 4) Then
-                    respuesta = numero / 0.000109361 'Yarda
+                    respuesta = numero * 0.000109361 'Yarda
                 ElseIf (opcionEquivalencia = 5) Then
-                    respuesta = numero / 0.001 'Metro
+                    respuesta = numero * 0.001 'Metro
                 ElseIf (opcionEquivalencia = 6) Then
-                    respuesta = numero / 0.000001 'Kilometro
+                    respuesta = numero * 0.000001 'Kilometro
                 End If
 
             Case 1 'Centimetro
                 If (opcionEquivalencia = 0) Then
-                    respuesta = numero / 10 'milimetro 
+                    respuesta = numero * 10 'milimetro 
                 ElseIf (opcionEquivalencia = 1) Then
                     respuesta = numero * 1 'Centimetro
                 ElseIf (opcionEquivalencia = 2) Then
-                    respuesta = numero / 0.3937 'Pulgada
+                    respuesta = numero * 0.3937 'Pulgada
                 ElseIf (opcionEquivalencia = 3) Then
-                    respuesta = numero / 0.0328 'Pie
+                    respuesta = numero * 0.0328 'Pie
                 ElseIf (opcionEquivalencia = 4) Then
-                    respuesta = numero / 0.0109 'Yarda
+                    respuesta = numero * 0.0109 'Yarda
                 ElseIf (opcionEquivalencia = 5) Then
-                    respuesta = numero / 0.01 'Metro
+                    respuesta = numero * 0.01 'Metro
                 ElseIf (opcionEquivalencia = 6) Then
-                    respuesta = numero / 0.00001 'Kilometro
+                    respuesta = numero * 0.00001 'Kilometro
                 End If
 
             Case 2 'Pulgada
                 If (opcionEquivalencia = 0) Then
-                    respuesta = numero / 25.4 'Milimetro
+                    respuesta = numero * 25.4 'Milimetro
                 ElseIf (opcionEquivalencia = 1) Then
-                    respuesta = numero / 2.54 'centimetro
+                    respuesta = numero * 2.54 'centimetro
                 ElseIf (opcionEquivalencia = 2) Then
-                    respuesta = numero / 0.3937 'Pulgada
+                    respuesta = numero * 0.3937 'Pulgada
                 ElseIf (opcionEquivalencia = 3) Then
-                    respuesta = numero / 0.0328 'Pie
+                    respuesta = numero * 0.0328 'Pie
                 ElseIf (opcionEquivalencia = 4) Then
-                    respuesta = numero / 0.0109 'Yarda
+                    respuesta = numero * 0.0109 'Yarda
                 ElseIf (opcionEquivalencia = 5) Then
-                    respuesta = numero / 0.01 'Metro
+                    respuesta = numero * 0.01 'Metro
                 ElseIf (opcionEquivalencia = 6) Then
-                    respuesta = numero / 0.0000254 'Kilometro
+                    respuesta = numero * 0.0000254 'Kilometro
                 End If
 
             Case 3 'Pie
                 If (opcionEquivalencia = 0) Then
-                    respuesta = numero / 304.8 'Milimetro
+                    respuesta = numero * 304.8 'Milimetro
                 ElseIf (opcionEquivalencia = 1) Then
-                    respuesta = numero / 30.48 'Centimetro
+                    respuesta = numero * 30.48 'Centimetro
                 ElseIf (opcionEquivalencia = 2) Then
                     respuesta = numero * 1 'Pie
                 ElseIf (opcionEquivalencia = 4) Then
-                    respuesta = numero / 0.3333 'Yarda
+                    respuesta = numero * 0.3333 'Yarda
                 ElseIf (opcionEquivalencia = 5) Then
-                    respuesta = numero / 0.3048 'Metro
+                    respuesta = numero * 0.3048 'Metro
                 ElseIf (opcionEquivalencia = 6) Then
-                    respuesta = numero / 0.0003048 'Kilometro
+                    respuesta = numero * 0.0003048 'Kilometro
                 End If
 
             Case 4 'Yarda
                 If (opcionEquivalencia = 0) Then
-                    respuesta = numero / 914.4 'Milimetro
+                    respuesta = numero * 914.4 'Milimetro
                 ElseIf (opcionEquivalencia = 1) Then
-                    respuesta = numero / 91.44 'Centimetro
+                    respuesta = numero * 91.44 'Centimetro
                 ElseIf (opcionEquivalencia = 2) Then
-                    respuesta = numero / 3 'Pie
+                    respuesta = numero * 3 'Pie
                 ElseIf (opcionEquivalencia = 4) Then
                     respuesta = numero * 1 'Yarda
                 ElseIf (opcionEquivalencia = 5) Then
-                    respuesta = numero / 0.9144 'Metro
+                    respuesta = numero * 0.9144 'Metro
                 ElseIf (opcionEquivalencia = 6) Then
-                    respuesta = numero / 0.0009144 'Kilometro
+                    respuesta = numero * 0.0009144 'Kilometro
                 End If
 
             Case 5 'Metro
                 If (opcionEquivalencia = 0) Then
-                    respuesta = numero / 1000 'Milimetro
+                    respuesta = numero * 1000 'Milimetro
                 ElseIf (opcionEquivalencia = 1) Then
-                    respuesta = numero / 100 'Centimetro
+                    respuesta = numero * 100 'Centimetro
                 ElseIf (opcionEquivalencia = 2) Then
-                    respuesta = numero / 3.28084 'Pie
+                    respuesta = numero * 3.28084 'Pie
                 ElseIf (opcionEquivalencia = 4) Then
-                    respuesta = numero / 1.09361 'Yarda
+                    respuesta = numero * 1.09361 'Yarda
                 ElseIf (opcionEquivalencia = 5) Then
                     respuesta = numero * 1 'Metro
                 ElseIf (opcionEquivalencia = 6) Then
-                    respuesta = numero / 0.001 'Kilometro
+                    respuesta = numero * 0.001 'Kilometro
                 End If
 
             Case 6 'Kiometro
                 If (opcionEquivalencia = 0) Then
-                    respuesta = numero / 1000000 'Milimetro
+                    respuesta = numero * 1000000 'Milimetro
                 ElseIf (opcionEquivalencia = 1) Then
-                    respuesta = numero / 100000 'Centimetro
+                    respuesta = numero * 100000 'Centimetro
                 ElseIf (opcionEquivalencia = 2) Then
-                    respuesta = numero / 3280.84 'Pie
+                    respuesta = numero * 3280.84 'Pie
                 ElseIf (opcionEquivalencia = 4) Then
-                    respuesta = numero / 1093.61 'Yarda
+                    respuesta = numero * 1093.61 'Yarda
                 ElseIf (opcionEquivalencia = 5) Then
-                    respuesta = numero / 1000 'Metro
+                    respuesta = numero * 1000 'Metro
                 ElseIf (opcionEquivalencia = 6) Then
                     respuesta = numero * 1 'Kilometro
                 End If
